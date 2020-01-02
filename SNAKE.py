4acf7982655728f9ee0698228d26af0e6c22b6ba
programmez <francoistonic@bbox.fr>
2020-01-02 09:07:47 +0100

    Add files via upload

---
+++ SNAKE.py
@@ -0,0 +1,15 @@
+Ahmed Ouamel
+
+Peacek => paix
+
+
+Great11
+
+2002 => Montreal 
+
+ALGERIE => enseignement français langue secondaire primaire / secondaire 
+
+2006 2007 
+
+
+conda create -n test1 python=3
